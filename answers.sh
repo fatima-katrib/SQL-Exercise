d4ee67343ecc1ec65c7b9b9487a50454bdcbe42f
fatima-katrib <fatimakatrib19@gnail.com>
2022-12-12 21:12:43 +0200

    Joins

--- answers.sh
+++ answers.sh
@@ -20,3 +20,9 @@ SELECT name, Age, Gender, Points FROM students WHERE name = "Layal";
 UPDATE graduates SET Graduation = "2018-09-08" WHERE name = "Layal";
 DELETE FROM students WHERE name = "Layal";
 
+
+
+SELECT employees.name, employees.Company, companies.date FROM employees JOIN companies on employees.Company = companies.name;
+SELECT employees.name FROM employees Inner join companies ON employees.Company=companies.name WHERE companies.Date < 2000;
+SELECT companies.name FROM companies Inner JOIN employees ON employees.Company=companies.name WHERE employees.Role = "Graphic Designer";
+
